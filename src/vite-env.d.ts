ac3ce518bfa092f4338b7054528c32394c282efc
altohamikpaahi-blip <altohamikpaahi@gmail.com>
2026-08-11 08:19:44 +0200

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,3 +4,7 @@ declare module '*?url' {
   const src: string;
   export default src;
 }
+
+declare module 'mammoth/mammoth.browser' {
+  export function extractRawText(input: { arrayBuffer: ArrayBuffer }): Promise<{ value: string }>;
+}
